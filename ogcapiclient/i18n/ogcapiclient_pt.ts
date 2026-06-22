388f85fad35aef4321b3d242eca572e0d7018f76
Alexander Bruy <alexander.bruy@gmail.com>
2026-06-22 12:46:06 +0100

    .ts file

---
+++ ogcapiclient/i18n/ogcapiclient_pt.ts
@@ -0,0 +1,321 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DownloadTask</name>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="121" />
+        <source>Failed download data: {error}</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="144" />
+        <source>Downloading Features from collection '{name}'.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="160" />
+        <source>Instantiating vector layer.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="164" />
+        <source>An invalid layer created from URI '{uri}'.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="179" />
+        <source>Saving to GeoPackage.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="189" />
+        <source>Failed to save data: {message}.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="208" />
+        <source>Downloading Tiles from collection '{name}'.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="223" />
+        <source>Parsing tilesets.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="240" />
+        <source>Failed to create MBTiles file.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="284" />
+        <source>Saving to MBTiles.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/tasks/download_task.py" line="300" />
+        <source>Could not download tile '{url}': {error}</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>LayerPreparationTask</name>
+    <message>
+        <location filename="../qgis_backend/tasks/layer_preparation_task.py" line="74" />
+        <source>Failed to prepare online layer(s): {error}.</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>OgcApiClientDialog</name>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="75" />
+        <source>Title</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="78" />
+        <source>Name</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="81" />
+        <source>Type</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="84" />
+        <source>Abstract</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="131" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="95" />
+        <source>Add</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="129" />
+        <source>Download</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="157" />
+        <source>Empty URL</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="157" />
+        <source>Server URL is missed.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="449" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="324" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="189" />
+        <source>Canceled</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="449" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="324" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="189" />
+        <source>Operation was canceled.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="454" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="329" />
+        <location filename="../gui/ogc_api_client_dialog.py" line="194" />
+        <source>Error</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="195" />
+        <source>An error occured when connecting to the server. Check Message Log for more details.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="283" />
+        <source>No Layers</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="284" />
+        <source>Collections document contained no layers.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="330" />
+        <source>An error occured when loading layer(s). Check Message Log for more details.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="358" />
+        <source>No cache path</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="358" />
+        <source>Cache path is not set.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="383" />
+        <source>Cached data found</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="384" />
+        <source>Data for this collection has already been downloaded. Would you like to use the existing data?</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="405" />
+        <source>Large tile count</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="406" />
+        <source>Downloading selected area requires approximately{count} tiles. Do you want to proceed?</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../gui/ogc_api_client_dialog.py" line="455" />
+        <source>An error occured when downloading data. Check Message Log for more details.</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>OgcApiClientDialogBase</name>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>OGC API Client</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Cancel</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Authentication</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Choose or create an authentication configuration</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Configurations store encrypted credentials in the QGIS authentication storage.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Connect</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Mode</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Cache path</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Offline</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Online</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Area of interest</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../ui/ogc_api_client_dialog.ui" line="0" />
+        <source>Server URL</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>OgcApiClientPlugin</name>
+    <message>
+        <location filename="../plugin.py" line="48" />
+        <location filename="../plugin.py" line="47" />
+        <location filename="../plugin.py" line="42" />
+        <location filename="../plugin.py" line="41" />
+        <location filename="../plugin.py" line="32" />
+        <source>OGC API Client</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../plugin.py" line="37" />
+        <source>Help</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>OgcDiscoveryTask</name>
+    <message>
+        <location filename="../qgis_backend/tasks/ogc_discovery_task.py" line="62" />
+        <source>Failed to perform discovery of the OGC API server {url}: {error}.</source>
+        <translation type="unfinished" />
+    </message>
+</context><context>
+    <name>QgisLoader</name>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="48" />
+        <source>Failed to decode reply as UTF-8: {error}</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="51" />
+        <source>Failed to decode reply as UTF-8</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="59" />
+        <source>Failed to parse JSON: {error}
+Snippet: {snippet}</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="64" />
+        <source>Failed to parse JSON.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="143" />
+        <source>HTTP request failed for {url}: [{code}] - {message}.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="153" />
+        <source>HTTP request failed for {url}.</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="158" />
+        <source>Server returned error {code} for {url}.
+{message}</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="166" />
+        <source>Server returned error {code} for {url}</source>
+        <translation type="unfinished" />
+    </message>
+    <message>
+        <location filename="../qgis_backend/loader.py" line="175" />
+        <source>Server returned an empty response.</source>
+        <translation type="unfinished" />
+    </message>
+</context></TS>
